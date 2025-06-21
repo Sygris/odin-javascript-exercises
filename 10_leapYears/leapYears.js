2a823f8d08033fa7bb4946d388f3237738b5ba31
Sygris <tiago.a.vista@gmail.com>
2025-06-21 23:50:31 +0100

    Solved 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const isDivisibleByFour = year % 4 === 0
+  const isCentury = year % 100 === 0
+  const isDivisibleByFourHundred = year % 400 == 0
 
+  // If the input is diviible by 4 and is not a century or is divisible by 400 it's a leap year
+  if (isDivisibleByFour && (!isCentury || isDivisibleByFourHundred)) {
+    return true
+  }
+  else {
+    return false
+  }
 };
 
 // Do not edit below this line
