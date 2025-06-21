c7777e70387a1dc4aa08c43223375daecf6ff63e
Sygris <tiago.a.vista@gmail.com>
2025-06-21 16:47:49 +0100

    Solved 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
+/*
+  @param {string} text - String to be repeated
+  @param {times} int - Number of times for the string to be repeated
+*/
+const repeatString = function(string, times) {
 
+  // return string.repeat(times)
+
+  // Guard clause where it returns "ERROR" if times is a negative number
+  if (times < 0) {
+    return "ERROR"
+  }
+
+  // Test using loop 
+  let output = ""
+  for (let index = 0; index < times; index++) {
+    output += string
+   } 
+  return output
 };
 
 // Do not edit below this line
