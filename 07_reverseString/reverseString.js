ad1470551f9ccf58ce19c2eda347dafeef511181
Sygris <tiago.a.vista@gmail.com>
2025-06-21 18:18:01 +0100

    Solved 07_reverseStrings

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const joinStrings = require("../05_joinStrings/joinStrings");
 
+// @param {string} string - string to be reversed
+const reverseString = function(string) {
+  return string.split("").reverse().join("")
 };
 
 // Do not edit below this line
