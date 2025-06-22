07e850897e4dfc8a21a7a3d3826f01b17253c914
Sygris <tiago.a.vista@gmail.com>
2025-06-22 21:12:10 +0100

    Made the functions clearer

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,14 +1,11 @@
 const convertToCelsius = function(fahrenheit) {
   // Formula: C = (F - 32) X (5/9)
-  let result = (fahrenheit - 32) * (5/9)
-  // Rounded to one decimal place which needs to be converted to a number after
-  return parseFloat(result.toFixed(1))
+  return Math.round((fahrenheit - 32) * (5/9) * 10) / 10
 };
 
 const convertToFahrenheit = function(celsius) {
   // Formula: F = (C * 1.8) + 32
-  let result = (celsius * 1.8) + 32
-  return parseFloat(result.toFixed(1))
+  return Math.round(((celsius * 9) /5 + 32) * 10) / 10
 };
 
 // Do not edit below this line
