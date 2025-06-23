f2d7206db3fb8d386a55650b6b80a1245c3f1dac
Sygris <tiago.a.vista@gmail.com>
2025-06-23 15:47:10 +0100

    Solved 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // Regular Expression that looks only for letters and numbers
+  const regex = /[a-z0-9]/g
 
+  // Cleans the string (makes it lowercase and returns and array with the result of match())
+  const cleanedArray = string
+    .toLowerCase()
+    .match(regex)
+
+  // Checks if the array is null if so abort
+  if(!cleanedArray) return false
+
+  // Reverses the content of cleanedArray and copies it to reversedArray
+  // [...cleanedArray] creates a shallow copy of the cleanedArray to prevent .reverse() from affecting the cleanedArray variable
+  const reversedArray = [...cleanedArray].reverse()
+
+  return cleanedArray.join("") === reversedArray.join("")
 };
 
 // Do not edit below this line
